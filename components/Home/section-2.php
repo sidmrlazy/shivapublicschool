a2871e82d9f283bab707ed4fb6a3f71d36027183
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-01-25 18:49:39 +0530

    modified:   assets/css/styles.css new file:   assets/images/svg/icon-1.svg new file:   assets/images/svg/icon-2.svg new file:   assets/images/svg/icon-3.svg new file:   assets/images/svg/icon-4.svg new file:   assets/images/svg/icon-5.svg new file:   assets/images/svg/icon-6.svg new file:   assets/images/webp/banner-1.webp new file:   components/Home/section-1.php new file:   components/Home/section-2.php modified:   index.php

---
+++ components/Home/section-2.php
@@ -0,0 +1,31 @@
+<div class="section-2-grid">
+    <div class="section-2-card">
+        <img src="assets/images/svg/icon-1.svg" alt="">
+        <p>Computer Lab</p>
+    </div>
+
+    <div class="section-2-card">
+        <img src="assets/images/svg/icon-2.svg" alt="">
+        <p>Library</p>
+    </div>
+
+    <div class="section-2-card">
+        <img src="assets/images/svg/icon-3.svg" alt="">
+        <p>Play Ground</p>
+    </div>
+
+    <div class="section-2-card">
+        <img src="assets/images/svg/icon-4.svg" alt="">
+        <p>Chemistry Lab</p>
+    </div>
+
+    <div class="section-2-card">
+        <img src="assets/images/svg/icon-5.svg" alt="">
+        <p>Physics Lab</p>
+    </div>
+
+    <div class="section-2-card">
+        <img src="assets/images/svg/icon-6.svg" alt="">
+        <p>Biology Lab</p>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 $title = "Shiva Public School | Best School in Tiloi";
 include('components/header.php') ?>
 <?php include('components/navbar.php') ?>
+<?php include('components/Home/section-1.php') ?>
+<?php include('components/Home/section-2.php') ?>
 <?php include('components/footer.php') ?>
