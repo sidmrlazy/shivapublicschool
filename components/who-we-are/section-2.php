d883ec54c36b55c983cd7c8dd644f2a7b4fa86eb
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-02-12 11:08:23 +0530

    modified:   components/who-we-are/section-2.php modified:   components/who-we-are/section-3.php

--- components/who-we-are/section-2.php
+++ components/who-we-are/section-2.php
@@ -1,7 +1,20 @@
 <div id="about-us" class="container mt-5 mb-5">
     <h2>About Us</h2>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quaerat placeat voluptatum vero, porro esse
-        corrupti dolorem eius tempore pariatur! Veniam earum est dignissimos sit soluta ab ad ex dolore eveniet fugiat,
-        sapiente necessitatibus deserunt eius illo harum amet optio molestiae dicta ratione adipisci? Praesentium nemo
-        laudantium ratione doloremque dolores.</p>
+    <p>The Tiloi wing of Shiva Public School is recognised by Anglo Indian School on I.C.S.E. pattern, and directly
+        managed by Shiva Educational Society. Carrying forward the legacy of the Honorable Late. Shri Sant Ram Singh the
+        Tiloi Branch boasts of an impressive building and infrastructure and well
+        equipped classrooms, library, laboratories, music, dance and sports room. The emphasis is on embracing
+        technology and preparing children for the 21st century through global collaborations, experiential learning and
+        activity based interactive sessions.
+    </p>
+
+    <p>
+        The highly experienced and sensitive teachers constantly update themselves
+        through webinars, professional development programs and global certifications. There is a strong emphasis on
+        child centered learning and individual monitoring. The school has the unique distinction of conducting powerful
+        and inspirational morning Assemblies, aimed at character building & equipping children with core life skills. A
+        comprehensive set of co-curricular activities and hobby clubs focus on the holistic development of each child.
+        other.
+
+    </p>
 </div>
--- components/who-we-are/section-3.php
+++ components/who-we-are/section-3.php
@@ -1,7 +1,24 @@
 <div id="principals-message" class="container mt-5 mb-5">
     <h2>Principal's Message</h2>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt quaerat placeat voluptatum vero, porro esse
-        corrupti dolorem eius tempore pariatur! Veniam earum est dignissimos sit soluta ab ad ex dolore eveniet fugiat,
-        sapiente necessitatibus deserunt eius illo harum amet optio molestiae dicta ratione adipisci? Praesentium nemo
-        laudantium ratione doloremque dolores.</p>
+    <p>Learning is a lifelong process and Shiva Public School, Tiloi - Amethi envisages the all-round development of
+        the
+        children and caters to the needs of the students dwelling in the picturesque surroundings of our campus.
+        Education is a joint venture and its attainment is a mutual undertaking in which Students, Teachers, parents
+        and the Principal strive together hand in hand to translate this aim into reality. I feel proud to embark on
+        a journey in which I would play a significant role in shaping the character of the future citizens of the
+        country.</p>
+
+    <p>In a fast changing world where change is probably only thing which is constant, it is high time we alter our
+        approach towards teaching-learning process and foresee the students a co-learner and respect them for their
+        individuality. Accommodating every individuality and respecting distinctiveness of every individual in the
+        process of learning is the order of the day and as the Principal of the Shiva Public School, it would be my
+        solemn duty to
+        ensure the same is achieved through innovative practices, not only in the administrative procedures, but also in
+        pedagogical approaches.</p>
+
+    <p>Out of various stake holders, teachers, in all probability, play the most pivotal role in the overall development
+        of the students’ personality. A teacher of calm temperament and disposition may prove to be an asset and I as
+        the head of this Vidyalaya, would provide them congenial and conducive environment to work together as a team to
+        bring best out the students and make them self-sufficient learners.
+        Through multifarious activities, the latent talent of the students would be brought to the fore.</p>
 </div>
