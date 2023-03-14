f9ec4b404c269bf8bd41c984836460d797a96e0a
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-03-14 13:17:39 +0530

    modified:   index.php

--- index.php
+++ index.php
@@ -1,35 +1,35 @@
 <?php
-//$title = "Under Development | Best School in Tiloi";
-//include('components/header.php')
+$title = "Under Development | Best School in Tiloi";
+include('components/header.php')
 ?>
-<?php //include('components/under-maintenance.php')
+<?php include('components/under-maintenance.php')
 ?>
 
 <?php
-$title = "Shiva Public School | Best School in Tiloi";
-include('components/header.php')
+// $title = "Shiva Public School | Best School in Tiloi";
+// include('components/header.php')
 ?>
-<?php include('components/navbar.php')
-?>
-
-
-<?php include('components/Home/section-1.php')
-?>
-<?php include('components/Home/section-4.php')
-?>
-<?php include('components/Home/section-2.php')
+<?php //include('components/navbar.php')
 ?>
 
-<?php include('components/Home/section-3.php')
+
+<?php // include('components/Home/section-1.php')
 ?>
-<?php include('components/Home/section-8.php')
+<?php // include('components/Home/section-4.php')
+?>
+<?php // include('components/Home/section-2.php')
 ?>
 
-<?php include('components/Home/section-5.php')
+<?php // include('components/Home/section-3.php')
 ?>
-<?php include('components/Home/section-6.php')
+<?php // include('components/Home/section-8.php')
 ?>
-<?php include('components/Home/section-7.php')
+
+<?php // include('components/Home/section-5.php')
 ?>
-<?php include('components/footer.php')
+<?php // include('components/Home/section-6.php')
+?>
+<?php // include('components/Home/section-7.php')
+?>
+<?php // include('components/footer.php')
 ?>
